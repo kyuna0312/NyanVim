1cb9f17bf551f1b151228ebb334dabdfc901b9a1
kyuna0312 <khatanzorigb@gmail.com>
2026-06-08 00:08:44 +0800

    refactor(plugins): curated ui.lua (nvim-tree, lualine, bufferline, illuminate, indent)

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,184 +1,41 @@
 return {
   {
-    "lukas-reineke/virt-column.nvim",
-    opts = {
-      char = { "┆" },
-      virtcolumn = "80",
-      highlight = { "NonText" },
-    },
-  },
-  {
-    "folke/noice.nvim",
-    event = "VeryLazy",
-    dependencies = {
-      "MunifTanjim/nui.nvim",
-      "rcarriga/nvim-notify",
-    },
+    "nvim-tree/nvim-tree.lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
-      require("noice").setup({
-        lsp = {
-          override = {
-            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-            ["vim.lsp.util.stylize_markdown"] = true,
-            ["cmp.entry.get_documentation"] = true,
-          },
-        },
-        presets = {
-          bottom_search = true,
-          command_palette = true,
-          long_message_to_split = true,
-          inc_rename = false,
-          lsp_doc_border = false,
-        },
+      require("nvim-tree").setup({
+        sort_by = "case_sensitive",
+        view = { width = 30 },
+        filters = { dotfiles = true },
       })
-    end
+    end,
   },
   {
-    "j-hui/fidget.nvim",
-    opts = {
-      notification = {
-        window = {
-          winblend = 0,
-          border = "rounded",
-        },
-      },
-    },
-  },
-  {
-    "rcarriga/nvim-notify",
-    opts = {
-      timeout = 500,
-      render = "compact",
-      max_height = function()
-        return math.floor(vim.o.lines * 0.75)
-      end,
-      max_width = function()
-        return math.floor(vim.o.columns * 0.25)
-      end,
-      on_open = function(win)
-        vim.api.nvim_win_set_config(win, { zindex = 100 })
-      end,
-    },
-  },
-  {
-    "b0o/incline.nvim",
-    event = "BufReadPre",
-    priority = 1200,
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
-      require("incline").setup({
-        highlight = {
-          groups = {
-            InclineNormal = { guibg = "#303270", guifg = "#a9b1d6" },
-            InclineNormalNC = { guibg = "none", guifg = "#a9b1d6" },
-          },
+      require("lualine").setup({
+        options = {
+          theme = "auto",
+          component_separators = "|",
+          section_separators = { left = "", right = "" },
+          globalstatus = true,
         },
-        window = { margin = { vertical = 0, horizontal = 1 } },
-        hide = { cursorline = true, only_win = true },
-        render = function(props)
-          local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
-          if vim.bo[props.buf].modified then
-            filename = "[*]" .. filename
-          end
-
-          local icon, color = require("nvim-web-devicons").get_icon_color(filename)
-
-          return { { icon, guifg = color }, { " " }, { filename } }
-        end,
       })
     end,
   },
   {
     "akinsho/bufferline.nvim",
-    opts = {
-      options = {
-        mode = "tabs",
-        show_buffer_close_icons = false,
-        show_close_icon = false,
-      },
-    },
-  },
-  {
-    "folke/edgy.nvim",
-    event = "VeryLazy",
-    init = function()
-      vim.opt.laststatus = 3
-      vim.opt.splitkeep = "screen"
-    end,
-    config = function()
-      require("edgy").setup({
-        bottom = {
-          {
-            ft = "toggleterm",
-            size = { height = 0.4 },
-          },
-          {
-            ft = "trouble",
-            size = { height = 0.4 },
-          },
-          {
-            ft = "qf",
-            title = "QuickFix",
-          },
-          {
-            ft = "help",
-            size = { height = 0.4 },
-          },
-        },
-        left = {
-          {
-            ft = "neo-tree",
-            size = { width = 0.2 },
-          },
-        },
-        right = {
-          {
-            ft = "aerial",
-            title = "Symbol Outline",
-            size = { width = 0.2 },
-          },
-        },
-      })
-    end,
-  },
-  {
-    "stevearc/aerial.nvim",
-    config = function()
-      require("aerial").setup({
-        layout = {
-          default_direction = "right",
-          placement = "edge",
-        },
-        attach_mode = "global",
-        backends = { "lsp", "treesitter", "markdown", "man" },
-        show_guides = true,
-      })
-    end,
-  },
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons",
-      "MunifTanjim/nui.nvim",
-    },
-    config = function()
-      require("neo-tree").setup({
-        close_if_last_window = true,
-        enable_git_status = true,
-        enable_diagnostics = true,
-        filesystem = {
-          follow_current_file = true,
-          use_libuv_file_watcher = true,
-        },
-      })
-    end,
-  },
-  {
-    "folke/trouble.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
-      require("trouble").setup()
+      require("bufferline").setup({
+        options = {
+          diagnostics = "nvim_lsp",
+          always_show_bufferline = false,
+        },
+      })
     end,
   },
+  { "RRethy/vim-illuminate" },
+  { "lukas-reineke/indent-blankline.nvim" },
 }

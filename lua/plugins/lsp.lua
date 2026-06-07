35efa57d424d5e13ae30acefa6d43a45e77753f7
kyuna0312 <khatanzorigb@gmail.com>
2026-06-08 00:17:25 +0800

    refactor(plugins): curated lsp.lua (mason + lspconfig vim.lsp.config + cmp)

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,65 +1,106 @@
--- Disable "No information available" notification on hover
--- and define border for hover window
-vim.lsp.handlers["textDocument/hover"] = function(_, result, ctx, config)
-  config = config or {
-    border = {
-      { "╭", "Comment" },
-      { "─", "Comment" },
-      { "╮", "Comment" },
-      { "│", "Comment" },
-      { "╯", "Comment" },
-      { "─", "Comment" },
-      { "╰", "Comment" },
-      { "│", "Comment" },
-    },
-  }
-  config.focus_id = ctx.method
-  if not (result and result.contents) then
-    return
-  end
-  local markdown_lines = vim.lsp.util.convert_input_to_markdown_lines(result.contents)
-  markdown_lines = vim.lsp.util.trim_empty_lines(markdown_lines)
-  if vim.tbl_isempty(markdown_lines) then
-    return
-  end
-  return vim.lsp.util.open_floating_preview(markdown_lines, "markdown", config)
-end
-
 return {
   {
     "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        vtsls = {
-          settings = {
-            typescript = {
-              inlayHints = {
-                enumMemberValues = { enabled = false },
-                functionLikeReturnTypes = { enabled = false },
-                parameterNames = { enabled = false },
-                parameterTypes = { enabled = false },
-                propertyDeclarationTypes = { enabled = false },
-                variableTypes = { enabled = false },
-              },
-            },
-          },
-        },
-        gopls = {
-          settings = {
-            gopls = {
-              hints = {
-                assignVariableTypes = false,
-                compositeLiteralFields = false,
-                compositeLiteralTypes = false,
-                constantValues = false,
-                functionTypeParameters = false,
-                parameterNames = false,
-                rangeVariableTypes = false,
-              },
-            },
-          },
-        },
-      },
+    dependencies = {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+      "hrsh7th/nvim-cmp",
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
+      "rafamadriz/friendly-snippets",
+      "onsails/lspkind.nvim",
     },
+    config = function()
+      require("mason").setup()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",
+          "pyright",
+          "ts_ls",
+          "rust_analyzer",
+          "gopls",
+          "jsonls",
+          "html",
+          "cssls",
+        },
+        automatic_installation = true,
+      })
+
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+      -- Enhanced LSP configuration (nvim 0.11+ API)
+      local servers = {
+        'lua_ls', 'pyright', 'ts_ls', 'rust_analyzer',
+        'gopls', 'jsonls', 'html', 'cssls'
+      }
+
+      -- Shared capabilities for every server
+      vim.lsp.config('*', {
+        capabilities = capabilities,
+      })
+
+      -- Server-specific settings
+      vim.lsp.config('lua_ls', {
+        settings = {
+          Lua = {
+            diagnostics = {
+              globals = { 'vim' },
+            },
+          },
+        },
+      })
+
+      vim.lsp.enable(servers)
+
+      -- Advanced completion setup
+      local cmp = require('cmp')
+      local luasnip = require('luasnip')
+      local lspkind = require('lspkind')
+
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        },
+        formatting = {
+          format = lspkind.cmp_format({
+            mode = 'symbol_text',
+            maxwidth = 50,
+            ellipsis_char = '...',
+          })
+        },
+        mapping = cmp.mapping.preset.insert({
+          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<C-e>'] = cmp.mapping.abort(),
+          ['<CR>'] = cmp.mapping.confirm({ select = true }),
+          ['<Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            else
+              fallback()
+            end
+          end, { 'i', 's' }),
+        }),
+        sources = cmp.config.sources({
+          { name = 'nvim_lsp' },
+          { name = 'luasnip' },
+          { name = 'path' },
+        }, {
+          { name = 'buffer' },
+        })
+      })
+    end
   },
 }

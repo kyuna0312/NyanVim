7416642c7b8bd3ae532a6b448e5976c33df17643
kyuna0312 <khatanzorigb@gmail.com>
2026-06-08 00:22:24 +0800

    refactor(plugins): add working treesitter.lua (master branch)

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,47 +1,20 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  event = { "BufReadPost", "BufNewFile" },
-  dependencies = {
-    "nvim-treesitter/nvim-treesitter-textobjects",
-    "windwp/nvim-ts-autotag",
-    "JoosepAlviste/nvim-ts-context-commentstring",
-    "nvim-treesitter/nvim-treesitter-context",
-  },
-  config = function()
-    require("nvim-treesitter.install").prefer_git = true
-
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = {
-        "lua", "vim", "vimdoc",
-        "bash", "markdown", "markdown_inline",
-        "javascript", "typescript", "python",
-        "html", "css", "json", "yaml",
-      },
-      sync_install = false,
-      auto_install = false,
-      ignore_install = {},
-      highlight = {
-        enable = true,
-        disable = function(_, bufnr)
-          return vim.api.nvim_buf_line_count(bufnr) > 50000
-        end,
-      },
-      indent = { enable = true },
-      autotag = { enable = true },
-      context_commentstring = { enable = true },
-      textobjects = {
-        select = {
-          enable = true,
-          lookahead = true,
-          keymaps = {
-            ["af"] = "@function.outer",
-            ["if"] = "@function.inner",
-            ["ac"] = "@class.outer",
-            ["ic"] = "@class.inner",
-          },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    branch = "master",
+    build = ":TSUpdate",
+    event = { "BufReadPost", "BufNewFile" },
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = {
+          "lua", "vim", "vimdoc", "python", "javascript",
+          "typescript", "tsx", "json", "html", "css",
+          "rust", "go", "bash", "markdown", "markdown_inline",
         },
-      },
-    })
-  end,
+        auto_install = true,
+        highlight = { enable = true },
+        indent = { enable = true },
+      })
+    end,
+  },
 }
